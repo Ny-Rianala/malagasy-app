0c2d7ce1f6036ffe5f233fbe210642fe676df281
student <kati.fan@onja.org>
2021-04-26 14:06:31 +0300

    added line of codes to the package and import cons

--- src/components/ActionButton/ActionButton.js
+++ src/components/ActionButton/ActionButton.js
@@ -1,15 +1,44 @@
 import * as React from 'react';
-import { SafeAreaView, Button,Image, Platform} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+import Correct from '../../assets/icons/correct.svg';
+// import FontAwesome from "react-native-vector-icons/FontAwesome";
+// import Constants from 'expo-constants';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'transparent',
+  },
+  text: {
+    color: '#06B6D4',
+    fontSize: 23,
+    borderWidth: 1,
+    width: '50%',
+    height: '50%',
+    margin: 20,
+    textAlign: 'center',
+    backgroundColor: 'transparent',
+    borderWidth: 0,
+  },
+  subcontainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginLeft: 0,
+  },
+  image: {
+    marginTop: 30,
+    marginLeft: 0,
+  },
+});
 
 export default function ActionButton({title, onPress}) {
-
-    return (
-    <SafeAreaView >
-        <Button
-            onPress={onPress}
-            title = {title}
-            type = "clear"
-            />
-    </SafeAreaView>
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPress} style={styles.subcontainer}>
+        <Text style={styles.text}>{title}</Text>
+        {/* <FontAwesome name={"chevron-up"} color="red" fontSize= {48}/> */}
+        <Correct />
+      </TouchableOpacity>
+    </View>
   );
 }
