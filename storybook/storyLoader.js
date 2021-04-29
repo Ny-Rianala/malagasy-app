469a7681a479f96874be67ce10964f396a183376
student <kati.fan@onja.org>
2021-04-29 20:12:29 +0300

    added language switcher button

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -6,6 +6,7 @@
 function loadStories() {
   require('../src/components/ActionButton/ActionButton.stories');
   require('../src/components/Example/Example.stories');
+  require('../src/components/LanguageSwitcherButton/LanguageSwitcherButton.stories');
   require('../src/components/List/List.stories');
   require('../src/components/ListItem/ListItem.stories');
   require('../src/components/NextButton/NextButton.stories');
@@ -20,6 +21,7 @@ function loadStories() {
 const stories = [
   '../src/components/ActionButton/ActionButton.stories',
   '../src/components/Example/Example.stories',
+  '../src/components/LanguageSwitcherButton/LanguageSwitcherButton.stories',
   '../src/components/List/List.stories',
   '../src/components/ListItem/ListItem.stories',
   '../src/components/NextButton/NextButton.stories',
