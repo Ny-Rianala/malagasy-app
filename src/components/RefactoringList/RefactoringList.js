110204d516433b6078496dbde984baddb8cf5305
student <kati.fan@onja.org>
2021-05-04 14:44:42 +0300

    added list

--- src/components/RefactoringList/RefactoringList.js
+++ src/components/RefactoringList/RefactoringList.js
@@ -1,11 +1,45 @@
-// components/Task.js
 import * as React from 'react';
-import {Text, SafeAreaView, FlatList, StyleSheet} from 'react-native';
+import {Text, SafeAreaView, StyleSheet, View} from 'react-native';
+import Learn from '../../assets/icons/learnt.svg';
 
-export default function RefactoringList({renderItem, data = () => null}) {
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  subContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+    color: '#06B6D4',
+    marginRight: 10,
+  },
+  icon: {
+    marginTop: 4,
+  },
+});
+
+export default function RefactoringList({
+  renderItem,
+  onPress,
+  data = () => null,
+}) {
   return (
-    <SafeAreaView>
-      <FlatList renderItem={renderItem}>{data}</FlatList>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text>{data}</Text>
+        <View
+          style={styles.subContainer}
+          renderItem={renderItem}
+          onPress={onPress}>
+          <Text style={styles.text}>Learn</Text>
+          <Learn style={styles.icon} />
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
--- src/components/RefactoringList/RefactoringList.stories.js
+++ src/components/RefactoringList/RefactoringList.stories.js
@@ -5,36 +5,45 @@ import {
   FlatList,
   SafeAreaView,
   TouchableOpacity,
+  StyleSheet,
 } from 'react-native';
 
 import {storiesOf} from '@storybook/react-native';
+import {categories} from '../../data/categories.json';
 
-import categories from '../../data/categories.json';
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'white',
+    paddingTop: 17,
+    paddingLeft: 16,
+    paddingBottom: 17,
+    paddingRight: 20,
+    borderWidth: 1,
+    borderColor: '#E5E5E5',
+  },
+});
 
-const DATA = [JSON.stringify(categories)];
+const DATA = categories;
 
-const Item = ({item}) => (
-  <TouchableOpacity>
-    <Text>{item.name}</Text>
-  </TouchableOpacity>
-);
-
-function RefactoringLists() {
-  const keyExtractor = (item, index) => index.toString();
+const RefactoringLists = () => {
   const renderItem = ({item}) => {
-    return <Item item={item} />;
+    return (
+      <TouchableOpacity style={styles.button} onPress={() => alert('pressed')}>
+        <Text>{item.name.en}</Text>
+      </TouchableOpacity>
+    );
   };
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{padding: 20}}>
       <FlatList
         data={DATA}
         renderItem={renderItem}
-        keyExtractor={this.keyExtractor}
+        keyExtractor={item => item.id}
       />
     </SafeAreaView>
   );
-}
+};
 
 storiesOf('RefactoringList', module)
   .addDecorator(story => <View>{story()}</View>)
