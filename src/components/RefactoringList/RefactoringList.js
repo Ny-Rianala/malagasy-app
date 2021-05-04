66f2471ad8836cc93f85f2ba0876abe9d0b2f3d9
student <kati.fan@onja.org>
2021-05-04 17:48:59 +0300

    added styled to the list

--- src/components/RefactoringList/RefactoringList.js
+++ src/components/RefactoringList/RefactoringList.js
@@ -1,27 +1,5 @@
 import * as React from 'react';
-import {Text, SafeAreaView, StyleSheet, View} from 'react-native';
-import Learn from '../../assets/icons/learnt.svg';
-
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  subContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  text: {
-    fontSize: 16,
-    color: '#06B6D4',
-    marginRight: 10,
-  },
-  icon: {
-    marginTop: 4,
-  },
-});
+import {SafeAreaView, TouchableOpacity, View, Text} from 'react-native';
 
 export default function RefactoringList({
   renderItem,
@@ -32,13 +10,9 @@ export default function RefactoringList({
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
         <Text>{data}</Text>
-        <View
-          style={styles.subContainer}
+        <TouchableOpacity
           renderItem={renderItem}
-          onPress={onPress}>
-          <Text style={styles.text}>Learn</Text>
-          <Learn style={styles.icon} />
-        </View>
+          onPress={onPress}></TouchableOpacity>
       </View>
     </SafeAreaView>
   );
--- src/components/RefactoringList/RefactoringList.stories.js
+++ src/components/RefactoringList/RefactoringList.stories.js
@@ -1,15 +1,16 @@
 import * as React from 'react';
 import {
-  View,
-  Text,
-  FlatList,
   SafeAreaView,
   TouchableOpacity,
+  FlatList,
+  View,
+  Text,
   StyleSheet,
 } from 'react-native';
 
 import {storiesOf} from '@storybook/react-native';
 import {categories} from '../../data/categories.json';
+import Learn from '../../assets/icons/arrow-right.svg';
 
 const styles = StyleSheet.create({
   button: {
@@ -20,6 +21,23 @@ const styles = StyleSheet.create({
     paddingRight: 20,
     borderWidth: 1,
     borderColor: '#E5E5E5',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  subContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+    color: '#06B6D4',
+    marginRight: 10,
+  },
+  icon: {
+    marginTop: 4,
+    color: '#06B6D4',
   },
 });
 
@@ -30,6 +48,10 @@ const RefactoringLists = () => {
     return (
       <TouchableOpacity style={styles.button} onPress={() => alert('pressed')}>
         <Text>{item.name.en}</Text>
+        <View style={styles.subContainer}>
+          <Text style={styles.text}>Learn</Text>
+          <Learn style={styles.icon} />
+        </View>
       </TouchableOpacity>
     );
   };
